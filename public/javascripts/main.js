660ccf0dc7449669ca3ba089c2a4542904b088d6
Ami Friedman <amishosh@gmail.com>
2019-01-03 08:03:52 +0200

    Improve new budget form to support dynamic addition of content

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,6 +1,48 @@
-$(".tab").click(function (event){
-    $(".active").toggleClass("active");
-    $(this).toggleClass("active");
+//Global variables holding the number of rows in the budget form
+let index1 = 1;
+let index2 = 1;
+let index3 = 1;
 
-})
+//Budget form handler
+$("#ul-category-1").on("click", ".edit-button" ,function(event){
+    toggleAddRemoveItem($(this), 1, index1++);
+});
+
+$("#ul-category-2").on("click", ".edit-button" ,function(event){
+    toggleAddRemoveItem($(this), 2, index2++);
+});
+
+$("#ul-category-3").on("click", ".edit-button" ,function(event){
+    toggleAddRemoveItem($(this), 3, index3++);
+});
+
+function toggleAddRemoveItem(item, category, index){
+    item.toggleClass("btn-success");
+    item.toggleClass("btn-danger");
+    if (item.text() === '+'){
+        item.text("-");
+        addRow(item, category, index);
+    } else {
+        removeRow(item);
+    }
+
+}
+
+function addRow(currentRow, category, index){
+    var newRow = $("<li style='list-style: none;'>");
+    var cols = '';
+
+    cols += `<div class='form-row'><div class='col form-group'><input type='text' class='form-control' name='${category}[${index}][name]' placeholder='Expense Name'></div>`;
+    cols += `<div class='col form-group'><input type='text' class='form-control' name='${category}[${index}][amount]' placeholder='Expense Amount'></div>`;
+    cols += `<div class='col form-group add-remove-button'><button type='button' class='btn btn-success edit-button'>+</button></div></div>`;
+
+    newRow.append(cols);
+    newRow.append("</li>");
+    
+    currentRow.closest("ul").append(newRow);
+}
+
+function removeRow(item){
+    item.closest("li").remove();
+}
 
