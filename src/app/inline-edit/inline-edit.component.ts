fbc7b7b0b6adfe48c575880ae6f47b51559bd4ca
Ami Friedman <ami.y.friedman@gmail.com>
2019-04-16 12:42:20 +0300

    Improve total style

--- src/app/inline-edit/inline-edit.component.ts
+++ src/app/inline-edit/inline-edit.component.ts
@@ -8,7 +8,6 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 export class InlineEditComponent {
 
   @Input('initialValue') value;
-  @Input('type') type = 'text';
   @Output() valueChange = new EventEmitter();
 
   isEditMode = false;
