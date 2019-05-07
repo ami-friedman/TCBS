41714e7446a8fde3d2ca9db7700e16dcd0bfd6c4
Ami Friedman <ami.y.friedman@gmail.com>
2019-05-07 15:44:16 +0300

    Persis expenses

--- src/app/expense/expense.component.ts
+++ src/app/expense/expense.component.ts
@@ -4,6 +4,7 @@ import { BudgetService } from '../services/budget.service';
 import { Subscription } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
 import { Expense } from '../modules/expenses';
+import { ExpenseService } from '../services/expense.service';
 
 @Component({
   selector: 'expense',
@@ -18,29 +19,47 @@ export class ExpenseComponent {
 
   private userId = '1234';
   private year;
+  private month;
 
-  subscription: Subscription;
+  budgetSub: Subscription;
+  expenseSub: Subscription;
 
-  constructor(private budgetService: BudgetService, route: ActivatedRoute) { 
-    this.initExpense();
+  constructor(private budgetService: BudgetService, private expenseService: ExpenseService ,route: ActivatedRoute) { 
     this.year = this.year = route.snapshot.paramMap.get('year') || new Date().getFullYear();
+    this.month = route.snapshot.paramMap.get('month') || new Date().getMonth();
   }
 
   ngOnInit() {
-    this.subscription = this.budgetService.get(this.userId, this.year)
+    this.budgetSub = this.budgetService.get(this.userId, this.year)
     .subscribe( budget => {
       if (!budget) this.budgetExists = false;
 
       this.budget = budget;
+    });
+
+    this.expenseSub = this.expenseService.get(this.userId, this.month, this.year)
+    .subscribe( expense => {
+      if (!expense) {
+        // This will re-trigger the observable - consider improving
+        this.create();
+      }
+      this.expense = expense;
     }); 
   }
 
+  private create() {
+    this.initExpense();
+    this.expenseService.create(this.userId, this.month, this.year, this.expense);    
+   }
+
   ngOnDestroy() {
-    this.subscription.unsubscribe();
+    this.budgetSub.unsubscribe();
+    this.expenseSub.unsubscribe();
   }
 
   update() {
-
+    console.log('called');
+    this.expenseService.update(this.userId, this.month, this.year, this.expense);
   }
 
   private initExpense() {
